172c05fb4f4fa4476824ca36ca05266a277c472f
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-07-29 00:41:57 +0300

    envLocalpassword

--- config.js
+++
@@ -1,5 +0,0 @@
-const DB_HOST = "mongodb+srv://Mykhailo:vBADnnnXh7t9MA2u@cluster0.zkkdc5c.mongodb.net/db-contacts?retryWrites=true&w=majority";
-
-module.exports = {
-    DB_HOST,
-};
--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const mongoose = require('mongoose');
 
 const app = require('./app')
 
-const {DB_HOST} = process.env;
-
-console.log(process.env.DB_HOST)
+const {DB_HOST, PORT=3000} = process.env;
 
 mongoose.set('strictQuery', true);
 
 
 mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful")
   })
   .catch(error => {
