fc240cc8188b95010760de091a9ac685891b7208
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-07-28 23:46:25 +0300

    configHidePassword

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const app = require('./app')
 
-const DB_HOST = "mongodb+srv://Mykhailo:vBADnnnXh7t9MA2u@cluster0.zkkdc5c.mongodb.net/db-contacts?retryWrites=true&w=majority"
+const  DB_HOST = require('./config')
 
 mongoose.set('strictQuery', true);
 
