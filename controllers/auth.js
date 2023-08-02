650512e785dbee12f94a4de91d81ac1084cf34b5
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-08-03 00:06:01 +0300

    jsonToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,8 @@
 const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
+require("dotenv").config();
+const { SECRET_KEY } = process.env;
+
 const { User } = require("../models/user");
 
 const { HttpError, ctrlWrapper } = require("../helpers");
@@ -23,6 +27,29 @@ const register = async (req, res) => {
     })
 }
 
+const login = async (req, res) => {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+        throw HttpError(401, "Email or password invalid");
+    }
+    const passwordCompare = await bcrypt.compare(password, user.password);
+    if (!passwordCompare){
+        throw HttpError(401, "Email or password invalid");
+    }
+    
+    const payload = {
+        id: user._id,
+    }
+    
+    const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+    res.json({
+        token,
+    })
+}
+
 module.exports = {
-        register:ctrlWrapper(register),
+    register: ctrlWrapper(register),
+    login: ctrlWrapper(login),
 }
