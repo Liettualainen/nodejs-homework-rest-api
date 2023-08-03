a545d54c5ba8a41ca31ed557856ceb05617d6d04
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-08-03 17:23:33 +0300

    mistake

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -11,14 +11,14 @@ const authenticate = async (req, res, next) => {
     const { authorization = "" } = req.headers;
     const [bearer, token] = authorization.split(" ");
     if (bearer !== "Bearer") {
-        next(HttpError(401, "mistake"))
+        next(HttpError(401))
     }
 
     try {
         const { id } = jwt.verify(token, SECRET_KEY);
         const user = await User.findById(id);
         if (!user) {
-               next(HttpError(401));
+               next(HttpError(401, "mistake"));
         }
         next();
     }
