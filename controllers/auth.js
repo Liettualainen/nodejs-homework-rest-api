45cfb2ce6290d9c69205914af07f851f5eb03f07
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-08-03 23:20:24 +0300

    clearToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,7 +62,7 @@ const getCurrent = async(req, res) => {
 
 const logout = async (req, res) => {
     const {_id} = req.user;
-    await User.findByIdAndUpdate(_id, {token:""})
+    await User.findByIdAndUpdate(_id, { token: "" });
         res.status(204).json({
         "message": "Logout success"
     })
