8c0ba15a8e61a6dc80ffee306f5bc94f3be80a9e
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-08-09 21:35:26 +0300

    minorfix

--- helpers/resizeImage.js
+++ helpers/resizeImage.js
@@ -5,7 +5,7 @@ const  HttpError  = require("./HttpError");
 const resizeImage = async (imagePath) => {
   try {
     const image = await Jimp.read(imagePath);
-    image.resize(250, 250);
+    await image.resize(250, Jimp.AUTO);
     await image.writeAsync(imagePath);
   } catch (error) {
     throw HttpError(404);
