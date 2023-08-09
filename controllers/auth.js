c9a726feb60a01e27947df8dd4f0ef4f78f3279b
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-08-09 16:41:18 +0300

    filePictureInput

--- controllers/auth.js
+++ controllers/auth.js
@@ -79,7 +79,7 @@ const updateAvatar = async (req, res) => {
     const resultUpload = path.join(avatarsDir, originalname);
     await fs.rename(tempUpload, resultUpload);
     const avatarURL = path.join("avatars", originalname);
-    await User.findByIdAndUpdate(_id, { avatarURL });
+    await User.findByIdAndUpdate(_id, {avatarURL});
     res.json({
         avatarURL,
     })
