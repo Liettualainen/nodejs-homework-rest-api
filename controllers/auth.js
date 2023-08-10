47ac72bfbada0d4b2e4026e4780d76f107296979
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-08-10 22:25:17 +0300

    verificationEmail

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,33 @@
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 require("dotenv").config();
-const { User } = require("../models/user");
-const { HttpError, ctrlWrapper, resizeImage } = require("../helpers");
-const { SECRET_KEY } = process.env;
-
 const gravatar = require("gravatar");
-const Jimp = require("jimp");
-
 const fs = require("fs/promises");
 const path = require("path");
+const { nanoid} = require("nanoid");
+
+const { User } = require("../models/user");
+const { HttpError, ctrlWrapper, resizeImage, sendEmail } = require("../helpers");
+const { SECRET_KEY, BASE_URL } = process.env;
 const avatarsDir = path.join(__dirname,"../" ,"public", "avatars")
 
 const register = async(req, res) => {
-    
     const {email, password} = req.body;
     const user = await User.findOne({email});
-
     if (user) {
         throw HttpError(409, "Email already in use");
     }
-
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
+    const verificationToken = nanoid();
 
-    const newUser = await User.create({...req.body, password:hashPassword, avatarURL});
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationToken });
+    const verifyEmail = {
+        to: email,
+        subject: "Verify email",
+        html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click verify email</a>`
+    }
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
         email: newUser.email,
@@ -45,11 +48,8 @@ const login = async(req, res) => {
     const payload = {
         id: user._id,
     }
-    
     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-
     await User.findByIdAndUpdate(user._id, {token});
-
     res.json({
         token,
     })
@@ -88,9 +88,7 @@ const updateAvatar = async (req, res) => {
     const filename = `${_id}_${originalname}`;
     const resultUpload = path.join(avatarsDir, filename);
     await fs.rename(tempUpload, resultUpload);
-
     await resizeImage(resultUpload);
-    
     const avatarURL = path.join("avatars", filename);
     await User.findByIdAndUpdate(_id, {avatarURL});
     res.json({
--- helpers/index.js
+++ helpers/index.js
@@ -2,6 +2,6 @@ const  HttpError  = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const handleMongooseError = require("./handleMongooseError");
 const resizeImage = require("./resizeImage");
-const sendEmail = require("./sendEmail")
+const sendEmail = require("./sendEmail");
 
 module.exports = { HttpError, ctrlWrapper, handleMongooseError,  resizeImage, sendEmail}
